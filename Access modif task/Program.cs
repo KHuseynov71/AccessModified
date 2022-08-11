ab78c3b883e951c75680e48eb08c5d949d5dc945
unknown <kamranmh@code.edu.az>
2022-08-11 16:13:36 +0400

    Access modifi task

---
+++ Access modif task/Program.cs
@@ -0,0 +1,21 @@
+﻿using Access_modif_task.access_modifie;
+using System;
+
+namespace Access_modif_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Kamran = new User();
+            Console.WriteLine("Please enter \"Username\" (note that Username can be min 6 max 25 characters)");
+            Kamran.UserName = Console.ReadLine();
+            Console.WriteLine(Kamran.UserName);
+            Console.WriteLine("please enter \"Password\" (note that this Password can be min 8 max 25 characters and must contain at least 1 uppercase, 1 lowercase letter and a number)");
+            Kamran.Pasword = Console.ReadLine();
+            Console.WriteLine(Kamran.Pasword);
+        }
+
+
+    }
+}
---
+++ Access modif task/access modifie/User.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_modif_task.access_modifie
+{
+    class User
+    {
+        private string _userName;
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value.Length<= 25 && value.Length >= 6)
+                {
+                   
+                        _userName = value;
+                    
+                }
+            }
+        }
+        private string _pasword;
+        public string Pasword
+        {
+            get
+            {
+                return _pasword;
+            }
+            set
+            {
+                if (value.Length <= 25 && value.Length >= 8)
+                {
+                    if (HasDigit(value)==true && HasUpper(value)==true && HasLower(value)==true)
+                    {
+                     _pasword = value;
+                    }
+                   
+                }
+            }
+        }
+        public bool HasDigit(string value)
+        {
+            foreach (char item in value)
+            {
+                if (item>=48 && item<=57 )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasUpper(string value)
+        {
+            foreach (char item in value)
+            {
+                if (item>=65 && item<=90)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public  bool HasLower(string value)
+        {
+            foreach (char item in value)
+            {
+                if (item>=97 && item<=122)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public User()
+        {
+
+        }
+        public User(string UserName)
+        {
+            this.UserName = UserName;
+        }
+        public User(string UserName,string Pasword):this(UserName)
+        {
+            this.Pasword = Pasword;
+        }
+    }
+}
